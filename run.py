3da6dbf534c2e3e08de7ab473469f99075a91818
warmshao <wenshaoguo1026@gmail.com>
2024-07-15 09:30:19 +0800

    build from source

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ import os
 """
 :wq
 pip install onnxruntime-gpu -U --index-url=https://pkgs.dev.azure.com/onnxruntime/onnxruntime/_packaging/onnxruntime-cuda-12/pypi/simple
+
+./build.sh --parallel --build_shared_lib --use_cuda --cuda_version 12.1 --cuda_home /usr/local/cuda --cudnn_home /usr/local/cuda/ --config Release --build_wheel --skip_tests --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES="80" --cmake_extra_defines CMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc --allow_running_as_root
 """
 
 if __name__ == '__main__':
