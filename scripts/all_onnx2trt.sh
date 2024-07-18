cd6b4a8fde1e8a0efed9de04b1ddd4c6da746d1f
warmshao <wenshaoguo1026@gmail.com>
2024-07-18 08:58:59 +0800

    convert all onnx2trt in a script

---
+++ scripts/all_onnx2trt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# warping+spade model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/warping_spade-fix.onnx
+# landmark model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/landmark.onnx
+# motion_extractor model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/motion_extractor.onnx -p fp32
+# face_analysis model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/retinaface_det.onnx
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/face_2dpose_106.onnx
+# appearance_extractor model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/appearance_feature_extractor.onnx
+# stitching model
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/stitching.onnx
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/stitching_eye.onnx
+python scripts/onnx2trt.py -o ./checkpoints/liveportrait_onnx/stitching_lip.onnx
