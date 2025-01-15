e88c9b1ed4c88858bc9d2340987a1189ec732739
vvincent1234 <vincent19901026@gmail.com>
2025-01-15 19:44:10 +0800

    delete proxy

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 @echo off
 git fetch origin
 git reset --hard origin/master
-"venv\Scripts\pip.exe" install -r requirements_win.txt
+".\venv\python.exe" -m pip config unset global.proxy first
+".\venv\Scripts\pip.exe" install -r requirements_win.txt
 pause
